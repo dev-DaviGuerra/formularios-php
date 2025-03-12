286d11a7dba312d8e58dbd794b8931d17eec088d
DvGuerraa <davimguerra@gmail.com>
2025-03-12 12:42:29 -0300

    first commit

---
+++ form.php
@@ -0,0 +1,32 @@
+<?php
+require 'functions.php';
+$erro = null;
+$sucesso = null;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <h1> Formulário </h1>
+    <?php if(exibirErro($erro)) :?>
+    <p style="color:red">
+        <?= $erro; ?>
+    </p>
+    <?php endif; ?>
+    <?php if(exibirErro($sucesso)) : ?>
+    <p style="color:green">
+        <?= $sucesso; ?>
+    </p>
+    <?php endif; ?>
+    <form method="POST">
+        <input type="text" name= "texto" placeholder= "Digite o texto">
+        <input type="submit" value="Enviar">
+    </form>
+    
+</body>
+</html>
